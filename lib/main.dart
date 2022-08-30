88d394b089be35829608f4ccd2a1f85a09df0cf6
anshb7 <92212789+anshb7@users.noreply.github.com>
2022-08-30 11:30:02 +0530

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,9 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:intl/intl.dart';
-import 'package:to_do_app/addtask.dart';
+
 import 'package:to_do_app/task.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'addtask.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           colorScheme: const ColorScheme(
         brightness: Brightness.light,
-        primary: Colors.purpleAccent,
+        primary: Color.fromARGB(255, 207, 17, 221),
         secondary: Colors.black12,
         onPrimary: Colors.white,
         onSecondary: Colors.white,
@@ -51,9 +50,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController taskcontroller = TextEditingController();
   bool isSwitched = false;
-  String dropdownvalue = 'Work';
-  var items = ['Work', 'Home', 'Goal', 'Task'];
   bool ischecked = false;
   bool checked = false;
   List<Task> tasks = [];
@@ -70,7 +68,143 @@ class _MyHomePageState extends State<MyHomePage> {
           child: FloatingActionButton(
               onPressed: () {
                 showModalBottomSheet(
-                    context: context, builder: (context) => AddTask());
+                    context: context,
+                    builder: (context) => Material(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(20, 10, 0, 0),
+                                child: Text(
+                                  '''Create New Task''',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    decoration: TextDecoration.none,
+                                    fontFamily: "product",
+                                    fontSize: 60.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 30,
+                              ),
+                              TextField(
+                                controller: taskcontroller,
+                                decoration: InputDecoration(
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      style: BorderStyle.none,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                      style: BorderStyle.none,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  labelText: 'Task Name',
+                                  labelStyle: TextStyle(
+                                      fontSize: 15,
+                                      fontFamily: "product",
+                                      color: Colors.grey),
+                                  contentPadding: EdgeInsets.only(
+                                    left: 20,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 25,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  const Padding(
+                                    padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                                    child: Icon(
+                                      Icons.calendar_today,
+                                      color: Colors.yellow,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 30,
+                                  ),
+                                  Text(
+                                    DateFormat.MMMMEEEEd().format(date),
+                                    style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 25),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  const Padding(
+                                    padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+                                    child: Icon(
+                                      Icons.timer,
+                                      color: Colors.orange,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 30,
+                                  ),
+                                  Text(
+                                    DateFormat("hh:mm:ss a")
+                                        .format(DateTime.now()),
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                                    child: SizedBox(
+                                      width: 350,
+                                      height: 60,
+                                      child: ElevatedButton(
+                                        child: Text(
+                                          'CREATE TASK',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20),
+                                        ),
+                                        onPressed: () {
+                                          addtask();
+                                          setState(() {});
+                                        },
+                                        style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  Colors.black),
+                                          shape: MaterialStateProperty.all(
+                                              RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          5))),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ));
               },
               elevation: 25,
               backgroundColor: const Color.fromRGBO(153, 101, 222, 10),
@@ -161,5 +295,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
+  }
+
+  void addtask() {
+    tasks.add(Task(task: taskcontroller.text));
+    setState(() {});
   }
 }
